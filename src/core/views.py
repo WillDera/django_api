67d6859d2bad51bbc865f5fb5e08ec069d48e770
Godswill <30760648+WillDera@users.noreply.github.com>
2020-06-21 20:29:54 +0100

    Core
    - Set up app named core
    
    - set up views.py for testing
    
    - linked core.views with root app (djangoapi)

---
+++ src/core/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+# Create your views here.
+
+
+def test_view(request):
+    data = {
+        'name': 'John',
+        'age': 23
+    }
+    return JsonResponse(data)
