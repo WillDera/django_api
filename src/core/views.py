c445913468fac09a6fcd1239fae89365e8b728c7
Godswill <30760648+WillDera@users.noreply.github.com>
2020-06-21 21:13:27 +0100

    Djanfo Restframework
    creating class based view for get reques.

--- src/core/views.py
+++ src/core/views.py
@@ -2,11 +2,21 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 # Create your views here.
+# third party imports
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
-def test_view(request):
-    data = {
-        'name': 'John',
-        'age': 23
-    }
-    return JsonResponse(data)
+class TestView(APIView):
+    def get(self, request, *args, **kwargs):
+        data = {
+            'name': 'John',
+            'age': 23
+        }
+
+# def test_view(request):
+#     data = {
+#         'name': 'John',
+#         'age': 23
+#     }
+#     return JsonResponse(data)
