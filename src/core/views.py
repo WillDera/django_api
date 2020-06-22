f743e79ad24b39b22e9d16b31e3791e8eaaafcf2
Godswill <30760648+WillDera@users.noreply.github.com>
2020-06-22 22:31:39 +0100

    Update
    - Created a serializer
    
    - created a funtion to handle post request

--- src/core/views.py
+++ src/core/views.py
@@ -6,6 +6,9 @@ from django.http import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from .serializers import PostSerializer
+from .models import Post
+
 
 class TestView(APIView):
     def get(self, request, *args, **kwargs):
@@ -15,9 +18,10 @@ class TestView(APIView):
         }
         return Response(data)
 
-# def test_view(request):
-#     data = {
-#         'name': 'John',
-#         'age': 23
-#     }
-#     return JsonResponse(data)
+    def post(self, request, *args, **kwargs):
+        serializer = PostSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
