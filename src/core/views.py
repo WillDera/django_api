c9c95246d32d9cc752806b0578eedbbbe2b2189a
Godswill <30760648+WillDera@users.noreply.github.com>
2020-06-23 20:12:04 +0100

    Update
    - Added api authentication through rest_framework.authtoken app

--- src/core/views.py
+++ src/core/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 
 # Create your views here.
 # third party imports
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -11,6 +12,9 @@ from .models import Post
 
 
 class TestView(APIView):
+
+    permission_classes = (IsAuthenticated, )
+
     def get(self, request, *args, **kwargs):
         qs = Post.objects.all()
         serializer = PostSerializer(qs, many=True)
