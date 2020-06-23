031e3a14b72a630b8561b2eb0b61ceaa97553b09
Godswill <30760648+WillDera@users.noreply.github.com>
2020-06-23 21:49:37 +0100

    Update

--- src/core/views.py
+++ src/core/views.py
@@ -6,24 +6,33 @@ from django.http import JsonResponse
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import generics
 
 from .serializers import PostSerializer
 from .models import Post
 
 
-class TestView(APIView):
-
-    permission_classes = (IsAuthenticated, )
+class PostView(generics.GenericAPIView):
+    serializer_class = PostSerializer
+    query_set = Post.objects.all()
 
     def get(self, request, *args, **kwargs):
-        qs = Post.objects.all()
-        serializer = PostSerializer(qs, many=True)
-        return Response(serializer.data)
+        return self.list(self, request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        serializer = PostSerializer(data=request.data)
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
+# class TestView(APIView):
+
+#     permission_classes = (IsAuthenticated, )
+
+#     def get(self, request, *args, **kwargs):
+#         qs = Post.objects.all()
+#         serializer = PostSerializer(qs, many=True)
+#         return Response(serializer.data)
+
+#     def post(self, request, *args, **kwargs):
+#         serializer = PostSerializer(data=request.data)
+
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors)
